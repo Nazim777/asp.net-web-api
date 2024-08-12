ded399510ff3800f7ad58627517eff1b7b787721
Nazim777 <iamnazimhosssain@gmail.com>
2024-08-12 11:57:34 +0600

    linq uses

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using MyApi.Models;
 using MyApi.Services;
 
